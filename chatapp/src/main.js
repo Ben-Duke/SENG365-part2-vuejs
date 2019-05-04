826ce268e2b40d442995036e630d1355ebd76544
Benjamin Duke <bad38@uclive.ac.nz>
2019-05-04 16:23:28 +1200

    fleshing out base using lab 6

--- chatapp/src/main.js
+++ chatapp/src/main.js
@@ -1,7 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Home from './Home/Home.vue';
+import Register from './Register/Register.vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.emulateJSON = true
+
+const routes = [{
+        path:"/",
+        component: Home
+      },
+      {
+        path:"/register",
+        component: Register
+      }
+];
+
+const router = new VueRouter ({
+  routes : routes,
+  mode : 'history'
+});
 
 new Vue({
   el: '#app',
+  router : router,
   render: h => h(App)
 })
