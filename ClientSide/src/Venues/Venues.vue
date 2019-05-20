6d08e9cc5ae06761b7cccb5d4d0383779b3f32ff
Benjamin Duke <bad38@uclive.ac.nz>
2019-05-20 21:37:37 +1200

    Destination now works

--- ClientSide/src/Venues/Venues.vue
+++ ClientSide/src/Venues/Venues.vue
@@ -26,13 +26,32 @@
   </b-navbar>
 
 
+    <table>
 
+      <tr>
+        <input v-model="userEntry" placeholder="Search on">
+        <select v-model="sortBy">
+          <option disabled value="">Sort by</option>
+          <option>None</option>
+          <option>Sort by highest star rating</option>
+          <option>Sort by lowest star rating</option>
+          <option>Sort by highest cost rating</option>
+          <option>Sort by lowest cost rating</option>
+          <option>Sort by closest</option>
+          <option>Sort by farthest</option>
+        </select>
+        <span>Selected: {{sortBy}} </span>
+        <input v-model="maxCost" placeholder="Search for venues less than ">
 
-    <div class="container"><input v-model="userEntry" placeholder="Search on"><button v-on:click="this.getVenues">Search</button> </div>
+        <button v-on:click="this.getVenues">Search</button>
+      </tr>
+    </table>
+
     <div id="container"class="container"></div>
   <div id="responseDiv"></div>
     <table class="table" id="venueTable">
       <thead>
+
       <tr>
         <th scope="col"></th>
         <th scope="col">Venue name</th>
@@ -44,7 +63,7 @@
         <th scope="col">Description</th>
         <th scope="col">Latitude</th>
         <th scope="col">Longitude</th>
-        <th scope="col">Star Rating </th>
+        <th scope="col">Star Rating</th>
         <th scope="col">Cost Rating</th>
       </tr>
       </thead>
@@ -62,42 +81,31 @@
         return {
           userEntry: '',
           categories: [],
-          userLatitude: 0,
-          userLongitude:0,
+
           city:"",
           selected:"",
           sortBy:"",
+          minStar:"",
+          maxCost:""
         }
       },
       mounted() {
-        this.getCategories();
+        this.getCategoriesDefault();
         //this.getVenues();
       },
       methods: {
-        getVenues: function () {
-          console.log("Selected is " + this.selected);
+        getVenuesDefault: function () {
+          console.log("VenuesDefault");
           let urlParams = '';
 
-          if(this.selected !== ''){
-            urlParams+= "categoryId="+this.selected;
-          }
-          if(this.city !== ""){
-            urlParams += "city=" + this.city;
-          }
-          else if(this.userEntry !==''){
-            urlParams += 'q='+this.userEntry;
-          }
-
-          this.$http.get('http://localhost:4941/api/v1/venues?'+ urlParams)
+          console.log('url is '+ urlParams);
+          //sortBy=DISTANCE&reverseSort=true&myLatitude=-43.5216384&myLongitude=172.57758719999998
+          //this.$http.get('http://localhost:4941/api/v1/venues?'+ 'sortBy=COST_RATING&reverseSort=true')
+          this.$http.get('http://localhost:4941/api/v1/venues?'+ 'sortBy=DISTANCE&reverseSort=true&myLatitude=-43.5216384&myLongitude=172.57758719999998')
             .then(function (response) {
-
-              ///Refreshing the table here
-              console.log(document.getElementById('rowTarget'));
-              //document.getElementById('rowTarget').innerHTML='';
 
 
               let table = document.getElementById("venueTable");
-//or use :  var table = document.all.tableid;
               for(let i = table.rows.length - 1; i > 0; i--)
               {
                 table.deleteRow(i);
@@ -134,7 +142,7 @@
                   picture.setAttribute('src', 'http://localhost:4941/api/v1/venues/' + data['venueId'] + "/photos/"+ data['primaryPhoto'] );
 
                 }else{
-                  console.log("No image for " + data["venueName"] );
+                  //console.log("No image for " + data["venueName"] );
                   picture.setAttribute('src', 'http://www.bruttles.com/layout/images/NoPhotoDefault.png?1323807363');
                 }
 
@@ -179,7 +187,7 @@
                 venueInfoRow.appendChild(venueCostRating);
 
                 table.appendChild(venueInfoRow);
-                console.log("Object is " + JSON.stringify(response.data[i]));
+                //console.log("Object is " + JSON.stringify(response.data[i]));
               }
 
 
@@ -191,6 +199,172 @@
             });
 
 
+        },
+        getVenues: function () {
+          console.log("Venues");
+          let urlParams = '';
+
+          if(this.sortBy === 'Sort by distance'){
+            navigator.geolocation.getCurrentPosition(function(location) {
+              console.log(location.coords.latitude);
+              console.log(location.coords.longitude);
+              console.log(location.coords.accuracy);
+              urlParams += "sortBy=DISTANCE" + '&myLatitude=' + location.coords.latitude
+                + '&myLongitude=' + location.coords.longitude;
+            });
+
+
+          }else if(this.sortBy === 'Sort by highest star rating'){
+              urlParams += 'sortBy=STAR_RATING' + "&"
+          }
+          else if(this.sortBy === 'Sort by lowest star rating'){
+            urlParams += 'sortBy=STAR_RATING&reverseSort=true' + "&"
+          }
+          else if(this.sortBy === 'Sort by highest cost rating'){
+            urlParams += 'sortBy=COST_RATING&reverseSort=true'+ "&"
+          }
+          else if(this.sortBy === 'Sort by lowest cost rating'){
+            urlParams += 'sortBy=COST_RATING' + "&"
+          }
+          else if(this.sortBy === 'Sort by closest'){
+            navigator.geolocation.getCurrentPosition(function(location) {
+              console.log("closest");
+               console.log(location.coords.latitude);
+              console.log(location.coords.longitude);
+              console.log(location.coords.accuracy);
+              urlParams += '&sortBy=DISTANCE&myLatitude=' + location.coords.latitude+ '&myLongitude='+location.coords.longitude+'&';
+            })
+
+
+          }
+          else if(this.sortBy === 'Sort by farthest'){
+            navigator.geolocation.getCurrentPosition(function(location) {
+              console.log("far");
+              // console.log(location.coords.latitude);
+              // console.log(location.coords.longitude);
+              // console.log(location.coords.accuracy);
+
+              console.log('url is '+ urlParams);
+              urlParams += '&sortBy=DISTANCE&reverseSort=true&myLatitude=' + location.coords.latitude+ '&myLongitude='+location.coords.longitude+'&';
+            })
+
+
+          }
+
+          if(this.maxCost != ''){
+            urlParams += 'maxCostRating=' + number.parse(this.maxCost) + "&"
+          }
+
+          if(this.selected !== ''){
+            urlParams+= "categoryId="+this.selected+"&";
+          }
+          if(this.city !== ""){
+            urlParams += "city=" + this.city+"&";
+          }
+          else if(this.userEntry !==''){
+            urlParams += 'q='+this.userEntry+"&";
+          }
+
+
+          setTimeout(() => {
+            console.log('url is '+ urlParams);
+            this.$http.get('http://localhost:4941/api/v1/venues?' + urlParams)
+              .then(function (response) {
+
+
+                let table = document.getElementById("venueTable");
+                for (let i = table.rows.length - 1; i > 0; i--) {
+                  table.deleteRow(i);
+                }
+
+
+                for (let i = 0; i < response.data.length; i++) {
+                  //Data of the request but for the index of i
+                  let data = response.data[i];
+                  let stars;
+                  if (data["meanStarRating"] == null) {
+                    stars = 3;
+                  } else {
+                    stars = data["meanStarRating"];
+                  }
+
+                  let cost;
+                  if (data["modeCostRating"] == null) {
+                    cost = "Free";
+                  } else {
+                    cost = data["modeCostRating"];
+                  }
+
+                  let table = document.getElementById("venueTable");
+                  //create a div
+                  let venueInfoRow = document.createElement("tr");
+                  let venuepicture = document.createElement("span");
+
+                  let venuePicture = document.createElement("td");
+                  let picture = document.createElement('img');
+
+
+                  if (data['primaryPhoto']) {
+                    picture.setAttribute('src', 'http://localhost:4941/api/v1/venues/' + data['venueId'] + "/photos/" + data['primaryPhoto']);
+
+                  } else {
+                    //console.log("No image for " + data["venueName"] );
+                    picture.setAttribute('src', 'http://www.bruttles.com/layout/images/NoPhotoDefault.png?1323807363');
+                  }
+
+                  picture.setAttribute('alt', 'No Picture');
+                  picture.setAttribute('height', '100');
+                  picture.setAttribute('width', '100');
+                  venuePicture.appendChild(picture);
+                  venueInfoRow.appendChild(venuePicture);
+
+
+                  let venueName = document.createElement("td");
+                  venueName.innerText = data["venueName"];
+                  venueInfoRow.appendChild(venueName);
+
+                  let venueCategory = document.createElement("td");
+                  let catId = (data["categoryId"]);
+                  venueCategory.innerText = this.categories[catId - 1]['categoryName'];
+                  venueInfoRow.appendChild(venueCategory);
+
+                  let venueCity = document.createElement("td");
+                  venueCity.innerText = data["city"];
+                  venueInfoRow.appendChild(venueCity);
+
+                  let venueDescription = document.createElement("td");
+                  venueDescription.innerText = data["shortDescription"];
+                  venueInfoRow.appendChild(venueDescription);
+
+                  let venueLatitude = document.createElement("td");
+                  venueLatitude.innerText = data["latitude"];
+                  venueInfoRow.appendChild(venueLatitude);
+
+                  let venueLongitude = document.createElement("td");
+                  venueLongitude.innerText = data["longitude"];
+                  venueInfoRow.appendChild(venueLongitude);
+
+                  let venueStarRating = document.createElement("td");
+                  venueStarRating.innerText = stars;
+                  venueInfoRow.appendChild(venueStarRating);
+
+                  let venueCostRating = document.createElement("td");
+                  venueCostRating.innerText = cost;
+                  venueInfoRow.appendChild(venueCostRating);
+
+                  table.appendChild(venueInfoRow);
+                  //console.log("Object is " + JSON.stringify(response.data[i]));
+                }
+
+
+              }, function (error) {
+                console.log("fetching failed");
+                this.error = error;
+                this.errorFlag = true;
+
+              });
+
+          }, 100)
         },
         getCategories: function () {
           this.$http.get('http://localhost:4941/api/v1/categories',
@@ -216,6 +390,39 @@
               dropdown.appendChild(opt);
 
               this.getVenues();
+            }, function (error) {
+              console.log("fetching failed");
+              this.error = error;
+              this.errorFlag = true;
+
+            });
+        },
+        getCategoriesDefault: function () {
+
+
+          this.$http.get('http://localhost:4941/api/v1/categories',
+            JSON.stringify({ }))
+            .then(function (response) {
+
+
+
+              this.categories = response.data;
+              console.log("cats are " +  JSON.stringify( this.categories));
+              let dropdown = document.getElementById('catDropDown');
+              for(let i in this.categories){
+
+
+                let opt = document.createElement('option');
+                opt.appendChild( document.createTextNode(this.categories[i]['categoryName']) );
+                opt.value = this.categories[i]['categoryId'];
+                dropdown.appendChild(opt);
+              }
+              let opt = document.createElement('option');
+              opt.appendChild( document.createTextNode('None') );
+              opt.value = '';
+              dropdown.appendChild(opt);
+
+              this.getVenuesDefault();
             }, function (error) {
               console.log("fetching failed");
               this.error = error;
